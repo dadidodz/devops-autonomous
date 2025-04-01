2978e3d1467908b446107a7fa52204c2a1e4764c
dadidodz <dorian.blatiere@hotmail.fr>
2025-04-01 12:44:19 +0200

    cd

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">=1.3.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.47.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "810ee914-59e8-4b7d-b1d9-12e207c35a4a"
+}
